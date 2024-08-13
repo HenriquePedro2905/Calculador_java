981f602a1791fcbf2833a7346b4c3f794d444963
Pedro Henrique de Souza <23040@etecdracena.local>
2024-08-13 09:08:49 -0300

    first commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author 23040
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaCalculadora tela = new TelaCalculadora();
+        
+        tela.show();
+    }
+    
+}
